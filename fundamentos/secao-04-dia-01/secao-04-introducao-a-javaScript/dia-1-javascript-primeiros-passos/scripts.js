d88f5340f774502336213903ca6e94cfd4de8427
contatopolianamarques@gmail.com <contatopolianamarques@gmail.com>
2022-10-20 19:08:50 -0300

    exercicios 2 e 3

--- fundamentos/secao-04-dia-01/secao-04-introducao-a-javaScript/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/secao-04-dia-01/secao-04-introducao-a-javaScript/dia-1-javascript-primeiros-passos/scripts.js
@@ -1,10 +1,27 @@
-// Exercicio 1
-const a = 7
-const b = 3
+// // Exercicio 1
+const a = 27;
+const b = 33;
+const c = 28;
 
- console.log(a + b);
- console.log(a-b);
- console.log(a*b);
- console.log(a/b);
- console.log(a%b);
+//  console.log(a + b);
+//  console.log(a-b);
+//  console.log(a*b);
+//  console.log(a/b);
+//  console.log(a%b);
   
+// // Exercicio 2 
+// if(a > b){
+//     console.log("A é maior que B")
+// } else {
+//     console.log('B é maior que A')
+// };
+
+// Exercicio 3 
+if(a > b && a > c){
+    console.log('O maior numero é A')
+} else if(b > a && b > c){
+    console.log('O maior numero é B')
+} else {
+    console.log('O maior numero é C')
+}
+
