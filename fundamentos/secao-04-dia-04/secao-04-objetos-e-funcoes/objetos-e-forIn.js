502f85d07423144e8e06140a1fe0dcb68e8b863c
contatopolianamarques@gmail.com <contatopolianamarques@gmail.com>
2022-10-26 10:38:58 -0300

    exercicio tio patinhas e margarida

---
+++ fundamentos/secao-04-dia-04/secao-04-objetos-e-funcoes/objetos-e-forIn.js
@@ -0,0 +1,58 @@
+//  //Imprima no console uma mensagem de boas-vindas para a personagem acima, 
+//   //incluindo seu nome. Use a sintaxe meuObjeto.chave. Valor esperado no console:
+
+  let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
+  };
+
+//   console.log('Bem vinda, ' + info.personagem) 
+
+//   //Insira no objeto uma nova propriedade com o nome de chave
+//   // ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console. Use a sintaxe meuObjeto['chave'] = valor. Valor esperado no console:
+
+//   info['recorrente'] = 'Sim'
+//   console.log(info)
+
+//   //Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+
+//   for(let key in info){
+//     console.log(info[key])
+//   
+
+  //Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: ‘Tio Patinhas’, ‘Christmas on Bear Mountain, Dell’s Four Color Comics #178’, ‘O último MacPatinhas’, ‘Sim’. Então, imprima os valores de cada objeto juntos, de acordo com cada uma das chaves. 
+
+
+  let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell’s Four Color Comics #178",
+    nota: 'último MacPatinhas',
+    recorrente: 'Sim'
+  }
+  for(let keys in info){
+    if(keys === 'recorrente' && info[keys] === 'Sim' &&
+    info2[keys] === 'Sim'){
+      console.log('Ambos recorrentes') 
+    } else {
+      console.log(info[keys] + ' e ' + info2[keys])
+    }
+  }
+
+  //Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: “O livro favorito de Julia Pessoa se chama ‘O Pior Dia de Todos’”.
+
+//Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a essa chave um objeto contendo as seguintes informações:
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
