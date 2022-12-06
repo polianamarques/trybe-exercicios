e76568f461e1aa4e7ef644d375c2b7f19ac316ea
contatopolianamarques@gmail.com <contatopolianamarques@gmail.com>
2022-12-06 18:26:46 -0300

    exercicio 1 ok

--- fundamentos/secao-08-dia-04/exercise1.js
+++ fundamentos/secao-08-dia-04/exercise1.js
@@ -7,6 +7,6 @@ const rectangle2 = [3, 5];
 const rectangle3 = [6, 9];
 const rectangles = [rectangle1, rectangle2, rectangle3];
 
-rectangles.forEach((...rectangle) => {
-  console.log(rectangleArea(rectangles)); // Altere o parâmetro recebido por rectangleArea()
+rectangles.forEach((rectangle) => {
+  console.log(rectangleArea(...rectangle)); // Altere o parâmetro recebido por rectangleArea()
 });
