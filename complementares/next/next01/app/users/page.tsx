ed3b33c2369290e194135d334f4b5ff8880a1981
contatopolianamarques@gmail.com <contatopolianamarques@gmail.com>
2023-08-11 10:57:10 -0300

    renderizando usuarios

--- complementares/next/next01/app/users/page.tsx
+++ complementares/next/next01/app/users/page.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import getAllUsers from "@/lib/getAllUsers";
+import Link from "next/link";
 
 
 export const metadata: Metadata = {
@@ -6,8 +8,30 @@ export const metadata: Metadata = {
 }
 
 
-export default function UsersPage() {
+export default async function UsersPage() {
+      const usersData: Promise<User[]> = getAllUsers()
+      const users = await usersData
+      const content = (
+        <section>
+            <h2>
+                <Link href="/">
+                Back to Home </Link>
+            </h2>
+            <br/>
+            {users.map((user) => {
+                return (
+                    <>
+                    <p key={user.id}>
+                        <Link href={`/users/${user.id}`}>{user.name}
+                        </Link>
+                    </p>
+                    <br/>
+                    </>
+                )
+            })}
+        </section>
+      )
   return (
-    <div>page</div>
+   content
   )
 }
